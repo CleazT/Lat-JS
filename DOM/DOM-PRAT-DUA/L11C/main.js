03cda6ef821e9eaffa1dbb657d89181d1983880e
Dzaki <dzakigunawan34@gmail.com>
2024-02-04 20:16:38 +0700

    .11

--- DOM/DOM-PRAT-DUA/L11C/main.js
+++ DOM/DOM-PRAT-DUA/L11C/main.js
@@ -6,9 +6,10 @@ tombolTampilkan.addEventListener('click', function(){
 
     hasil.innerHTML = '';
 
+    // for(var i = 1; i <= inputBaris.value; i++) {
     for(var i = 1; i <= inputBaris.value; i++) {
 
-        for(var j = 1; j <= i; j++) {
+        for(var j = i; j <= inputBaris.value; j++) {
             var box = document.createElement('div');
             box.className = 'box';
             box.innerHTML = j;
